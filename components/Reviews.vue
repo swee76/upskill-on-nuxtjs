b401a97caea329ac4607d8e53fac04bd8cc78a16
swee76 <oshinikithma1213@gmail.com>
2022-12-19 16:55:42 +0530

    Review card

---
+++ components/Reviews.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h3>Customer Reviews</h3>
+    <div v-if="!$fetchState.pending">
+      <ReviewCard
+        v-for="reviewer in reviewers.results"
+        :key="reviewer.login.uuid"
+        :review="reviewer"
+      />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Reviews",
+  data() {
+    return {
+      reviewers: [],
+    };
+  },
+
+  async fetch() {
+    this.reviewers = await fetch("https://randomuser.me/api/?results=5").then(
+      (response) => response.json()
+    );
+  },
+};
+</script>
+
+<style scoped></style>
--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -48,7 +48,7 @@
           {{ product.description }}
         </p>
       </div>
-      <!-- <Reviews /> -->
+      <Reviews />
     </div>
     <div v-else class="container padding">
       <PageNotFound />
