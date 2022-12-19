121f27fd441d1265315f439cde28c1f065f72a86
swee76 <oshinikithma1213@gmail.com>
2022-12-19 11:23:42 +0530

    Create Large card display

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,22 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+  <div class="container">
+    <Hero />
+    <LargeCardDisplay
+      v-for="largeCard in largeCardInfo"
+      :key="largeCard.id"
+      :cardsSection="largeCard"
+    />
+  </div>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
+import { largeCardSections } from "@/assets/data.js";
 export default {
-  name: 'IndexPage',
-  components: {
-    Card
-  }
-}
+  name: "IndexPage",
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+    };
+  },
+};
 </script>
